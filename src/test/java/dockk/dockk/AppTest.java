b177ce7e62913c99a987d95d729a92cf8a6b3444
bavie <bavie@DESKTOP-QB35EEE>
2021-06-07 20:24:33 +0530

    Initial commit

---
+++ src/test/java/dockk/dockk/AppTest.java
@@ -0,0 +1,20 @@
+package dockk.dockk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
