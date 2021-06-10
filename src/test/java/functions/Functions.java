b4a692d8b7fbe990aa9347eda4d451d610a4031f
bavie <bavie@DESKTOP-QB35EEE>
2021-06-10 20:42:09 +0530

    Function class added

--- src/test/java/functions/Functions.java
+++ src/test/java/functions/Functions.java
@@ -1,14 +1,18 @@
 package functions;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Functions {
 	
-	WebDriverWait driver;
+	WebDriver driver;
 	public static WebDriverWait wait;
-	protected int implictyWaitTime = 10;
-	protected int theadSleep = 5000;
+	public static int implictyWaitTime = 10;
+	public static int expWaitTime = 90;
+	public static int theadSleep = 5000;
 	
-	//public void //
+	public void waitTime() {
+		wait = new WebDriverWait(driver, expWaitTime);
+	}
 	
 }
--- src/test/java/scenarios/Docker.java
+++ src/test/java/scenarios/Docker.java
@@ -7,17 +7,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.server.DriverFactory;
 import org.testng.annotations.Test;
 
+import functions.Functions;
 import functions.setUpTearDown;
+
+
 
 public class Docker extends setUpTearDown {
 
-	
+	Functions func = new Functions();	
 	@Test
 	public void dockerPage(){
 		
 		WebElement element = driver.findElement(By.xpath("//input[@value='store']"));
 		element.click();
-		
+		func.waitTime();
 		if(element.isSelected()==true) {
 			System.out.println("Great! The link is selected");
 		}
